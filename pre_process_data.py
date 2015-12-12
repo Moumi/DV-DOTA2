5efb364174288bbb832afdcf4157b2ea428068ff
PBrand <patrick.brand2@gmail.com>
2015-12-12 16:50:06 +0100

    Pre-processes the master distance of each match to a file called 'matchID_master-distance.js'.
    Former-commit-id: 51df1c8113433b280b9eb7689dcf93897b2b560e

--- pre_process_data.py
+++ pre_process_data.py
@@ -10,6 +10,7 @@ def get_entry(row, headers, value_ids):
 		entry[headers[i]] = row[i]
 	return entry
 
+# Read match data from csv
 matches = dict()
 for i in range(1,48):
 	if not (len(matches.keys()) < max_output_files):
@@ -29,11 +30,33 @@ for i in range(1,48):
                         else:
                             break
 
+# Write a matches.js file which contains the names of all matches (to be read by combobox).
 with open('data/matches.js', 'w') as outfile:
     outfile.write("data = ")
     json.dump(matches.keys(), outfile)
 
+# write match data of each match to a seperate file.
 for match in matches:
     with open('data/'+str(match)+'.js', 'w') as outfile:
         outfile.write("data = ")
         json.dump(matches[match], outfile)
+
+# Read master distance data from csvfile
+master_distance = dict()
+for match in matches:
+    master_distance[match] = []
+
+with open('master-distance/master-distance.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    headers = reader.next()
+    value_ids = range(0,len(headers))
+    value_ids.remove(0)
+    for row in reader:
+        if master_distance.has_key(row[0]):
+                master_distance[row[0]].append(get_entry(row, headers, value_ids))
+
+# write master-distance data of each match to a seperate file.
+for match in master_distance:
+    with open('data/'+str(match)+'_master-distance.js', 'w') as outfile:
+        outfile.write("distanceData = ")
+        json.dump(master_distance[match], outfile)
